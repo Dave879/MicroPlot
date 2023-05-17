ad543b83ecfc4829fa8c1088b96118b4f88b2310
Dave879 <dvd.rori879@gmail.com>
2023-05-17 23:50:15 +0200

    Changed message protocol, multiple lines in same graph supported

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,9 @@
 #define SERVO_PIN 0
 #define POT_PIN 15
 
+#define HEATMAP_SIZE 400
 DataFormatter fmt;
-uint32_t data[64] = {0};
+uint32_t data[HEATMAP_SIZE] = {0};
 
 void setup()
 {
@@ -22,33 +23,64 @@ void setup()
 
 int i = 0;
 int cycle = 0;
-StaticJsonDocument<5000> doc;
+StaticJsonDocument<170000> doc;
+JsonArray arr;
+bool toggle = false;
 void loop()
 {
-  
-/*
-  doc[fmt.AddLineGraph("Random graph", 0, 1000)] = rand() % 500 + 250;
 
-  doc[fmt.AddLineGraph("Sensor SX", 0, 2000)] = rand() % 2000;
+  doc[fmt.AddLineGraph("Random graph", 2)] = rand() % 500 + 250;
+
+  doc[fmt.AddLineGraph("Sensor DX", 2)] = rand() % 20;
+  doc[fmt.AddLineGraph("Test 1", 1)] = rand() % 2000-3000;
+  doc[fmt.AddLineGraph("Test 2", 1)] = rand() % 3000+50000;
+  doc[fmt.AddLineGraph("Test 3", 1)] = rand() % 300+5000;
+  doc[fmt.AddLineGraph("Test 4", 1)] = rand() % 6000-15000;
+  doc[fmt.AddLineGraph("Test 5", 1)] = rand() % 5000;
+  doc[fmt.AddLineGraph("Test 6", 1)] = rand() % 5300 - 9000;
+  doc[fmt.AddLineGraph("Test 7", 1)] = millis() % 7;
+  doc[fmt.AddLineGraph("Test 8", 1)] = rand() % 395 - 636;
+  doc[fmt.AddLineGraph("Test 9", 1)] = rand() % 30-3500;
+  doc[fmt.AddLineGraph("Test 10", 1)] = rand() % 2000-10000;
   doc[fmt.AddLineGraph("millis")] = millis();
-  JsonArray arr = doc.createNestedArray(fmt.AddHeatmap("Test Heatmap", 8, 8, 0, 1000));
 
-  for (size_t i = 0; i < 64; i++)
+  arr = doc.createNestedArray(fmt.AddHeatmap("Test Heatmap", sqrt(HEATMAP_SIZE), sqrt(HEATMAP_SIZE), 0, 1000));
+  /*
+  if (toggle)
   {
-    data[i] = rand() % 1000;
-    arr.add(data[i]);
+    toggle = false;
+    for (size_t i = 0; i < HEATMAP_SIZE; i++)
+    {
+      data[i] = i;
+      arr.add(data[i]);
+    }
   }
+  else
+  {
+    toggle = true;
+    for (int32_t i = 10000; i >= 0; i--)
+    {
+      data[i] = i;
+      arr.add(data[i]);
+    }
+  }
+  */
+     for (size_t i = 0; i < HEATMAP_SIZE; i++)
+    {
+      data[i] = rand() % 1000;
+      arr.add(data[i]);
+    }
+
 
   doc[fmt.AddRepeatedMessage()] = "Serial8 bits available: ";
   doc[fmt.AddRepeatedMessage()] = "Repeated message test: ";
   doc[fmt.AddPacketIndex()] = fmt.GetAndIncrementPacketIdx();
 
-
   fmt.ResetIdx();
   serializeJson(doc, Serial);
   doc.clear();
-*/
-  Serial.print("{\"0&i\":1}{\"0&i\":1}{\"0&i\":1}{\"0&i\":1}{\"0&i\":1}{\"0&i\":1}{\"0&i\":1}");
+
+  // Serial.print("{\"0&i\":1}{\"0&i\":1}{\"0&i\":1}{\"0&i\":1}{\"0&i\":1}{\"0&i\":1}{\"0&i\":1}");
   /*
     // uint16_t reading = analogRead(POT_PIN);
     while (Serial.available() == 0)
@@ -70,5 +102,5 @@ void loop()
   */
   // Serial.print(millis());
   // Serial.println(" millisecondi");
-  //cycle++;
+  // cycle++;
 }
